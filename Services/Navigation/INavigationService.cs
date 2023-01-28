2cce7b3c326bf7cd188d93f5fb1ed4d347f4da9e
rana <rana.hanis93@gmail.com>
2023-01-28 15:48:20 +0200

    Added Navigation service

---
+++ Services/Navigation/INavigationService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace bahbashApp.Services.Navigation
+{
+    public interface INavigationService
+    {
+        Task InitializeAsync();
+
+        Task NavigateToAsync(string route, IDictionary<string, object> routeParameters = null);
+
+        Task PopAsync();
+    }
+}
---
+++ Services/Navigation/NavigationService.cs
@@ -0,0 +1,21 @@
+﻿
+namespace bahbashApp.Services.Navigation
+{
+    internal class NavigationService : INavigationService
+    {
+        public Task InitializeAsync() =>
+        NavigateToAsync("//Login" );
+
+        public Task NavigateToAsync(string route, IDictionary<string, object> routeParameters = null)
+        {
+            var shellNavigation = new ShellNavigationState(route);
+
+            return routeParameters != null
+                ? Shell.Current.GoToAsync(shellNavigation, routeParameters)
+                : Shell.Current.GoToAsync(shellNavigation);
+        }
+
+        public Task PopAsync() =>
+            Shell.Current.GoToAsync("..");
+    }
+}
