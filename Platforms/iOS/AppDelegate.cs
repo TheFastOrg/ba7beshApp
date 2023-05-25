582196e83c2b9dfffb2150b52a908f50f8dbd204
Hamza Rabah <h.rabah@outlook.com>
2023-05-25 18:09:04 +0300

    Use .net android 7

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace bahbashApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
