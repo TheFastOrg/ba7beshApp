8310a29dffa03f66420a3eef0056432fc9e94730
Hamza Rabah <h.rabah@outlook.com>
2023-06-12 00:40:20 +0300

    Add right-to-left layout based on the device language

--- bahbashApp/AppShell.xaml.cs
+++ bahbashApp/AppShell.xaml.cs
@@ -1,4 +1,5 @@
-﻿using bahbashApp.Services.Navigation;
+﻿using System.Globalization;
+using bahbashApp.Services.Navigation;
 
 namespace bahbashApp;
 
@@ -7,19 +8,19 @@ public partial class AppShell : Shell
     private readonly INavigationService _navigationService;
 
     public AppShell(INavigationService navigationService)
-	{
+    {
         _navigationService = navigationService;
 
         InitializeComponent();
+        FlowDirection = CultureInfo.CurrentCulture.Name.StartsWith("ar")
+            ? FlowDirection.RightToLeft
+            : FlowDirection.LeftToRight;
     }
 
     protected override async void OnHandlerChanged()
     {
         base.OnHandlerChanged();
 
-        if (Handler is not null)
-        {
-            await _navigationService.InitializeAsync();
-        }
+        if (Handler is not null) await _navigationService.InitializeAsync();
     }
 }
